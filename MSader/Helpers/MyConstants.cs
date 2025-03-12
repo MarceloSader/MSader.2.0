f0d60948367b83ae627063822e69c5d9c4e99ca4
Marcelo Silva Sader <marcelo@sader.com.br>
2025-03-12 19:06:27 -0300

    Hide token

--- MSader/Helpers/MyConstants.cs
+++ MSader/Helpers/MyConstants.cs
@@ -2,6 +2,6 @@
 {
     public class MyConstants
     {
-        public static string openAIKey = "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";
+        public static string openAIKey = "";
     }
 }
